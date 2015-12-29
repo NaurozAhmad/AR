e1198ab7a3aad927bf928c29451f0de803186cda
Nauroz Ahmad <thenewdawn1994@hotmail.com>
2015-12-29 16:14:38 +0500

    created home grid

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,31 +1,109 @@
 'use strict';
 
-(function() {
+(function () {
 
-class MainController {
+	class MainController {
+		constructor($http, $scope) {
+			this.$http = $http;
+			this.awesomeThings = [];
+			this.$scope = $scope;
 
-  constructor($http) {
-    this.$http = $http;
-    this.awesomeThings = [];
+			console.log('hey there');
 
-    $http.get('/api/things').then(response => {
-      this.awesomeThings = response.data;
-    });
-  }
+			$scope.featured = [{
+				title: 'Stuff here',
+				description: 'More content',
+				image: 'Bazaar.png'
+			}, {
+				title: 'Stuff here',
+				description: 'More content',
+				image: 'Donations.png'
+			}, {
+				title: 'Stuff here',
+				description: 'More content',
+				image: 'Cricket.png'
+			}, {
+				title: 'Stuff here',
+				description: 'More content',
+				image: 'Dropbox.png'
+			}, {
+				title: 'Stuff here',
+				description: 'More content',
+				image: 'Key.png'
+			}, {
+				title: 'Stuff here',
+				description: 'More content',
+				image: 'Lexi.png'
+			}, {
+				title: 'Stuff here',
+				description: 'More content',
+				image: 'FinalFour.png'
+			}, {
+				title: 'Stuff here',
+				description: 'More content',
+				image: 'Football.png'
+			}, {
+				title: 'Stuff here',
+				description: 'More content',
+				image: 'ESPNFootball.png'
+			}, {
+				title: 'Stuff here',
+				description: 'More content',
+				image: 'Everpix.png'
+			}, {
+				title: 'Stuff here',
+				description: 'More content',
+				image: 'Feynman.png'
+			}, {
+				title: 'Stuff here',
+				description: 'More content',
+				image: 'Fox.png'
+			}, {
+				title: 'Stuff here',
+				description: 'More content',
+				image: 'Maher.png'
+			}, {
+				title: 'Stuff here',
+				description: 'More content',
+				image: 'Mindshare.png'
+			}, {
+				title: 'Stuff here',
+				description: 'More content',
+				image: 'Elephant.png'
+			}, {
+				title: 'Stuff here',
+				description: 'More content',
+				image: 'Bose.png'
+			}, {
+				title: 'Stuff here',
+				description: 'More content',
+				image: 'Hitch.png'
+			}, {
+				title: 'Stuff here',
+				description: 'More content',
+				image: 'Rights.png'
+			}]
 
-  addThing() {
-    if (this.newThing) {
-      this.$http.post('/api/things', { name: this.newThing });
-      this.newThing = '';
-    }
-  }
+			$http.get('/api/things').then(response => {
+				this.awesomeThings = response.data;
+			});
+		}
 
-  deleteThing(thing) {
-    this.$http.delete('/api/things/' + thing._id);
-  }
-}
+		addThing() {
+			if (this.newThing) {
+				this.$http.post('/api/things', {
+					name: this.newThing
+				});
+				this.newThing = '';
+			}
+		}
 
-angular.module('animationRepublicApp')
-  .controller('MainController', MainController);
+		deleteThing(thing) {
+			this.$http.delete('/api/things/' + thing._id);
+		}
+	}
+
+	angular.module('animationRepublicApp')
+		.controller('MainController', MainController);
 
 })();
